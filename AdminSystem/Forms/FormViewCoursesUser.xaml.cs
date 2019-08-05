27ddb9efbd4db91fcfc86104a3e2320abaca2cb3
MasterFatum <master_fatum@hotmail.com>
2019-08-05 16:32:06 +0300

    2

---
+++ AdminSystem/Forms/FormViewCoursesUser.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using BLL.Entities;
+
+namespace AdminSystem.Forms
+{
+    /// <summary>
+    /// Логика взаимодействия для FormViewCoursesUser.xaml
+    /// </summary>
+    public partial class FormViewCoursesUser : Window
+    {
+        public int Id { get; set; }
+
+
+        public FormViewCoursesUser(int id, string lastname, string firstname, string middlename)
+        {
+            InitializeComponent();
+
+            Id = id;
+
+            LblUsername.Content = String.Format($"{lastname} {firstname} {middlename}");
+        }
+
+        private void DataGridUserCourses_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
--- AdminSystem/MainWindow.xaml.cs
+++ AdminSystem/MainWindow.xaml.cs
@@ -1,19 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using AdminSystem.Forms;
 using BLL.Concrete;
-
+using BLL.Entities;
 
 namespace AdminSystem
 {
@@ -23,6 +13,18 @@ namespace AdminSystem
     public partial class MainWindow : Window
     {
         UserRepository userRepository = new UserRepository();
+
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Category { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int? Evaluation { get; set; }
+        public string Lastname { get; set; }
+        public string Firstname { get; set; }
+        public string Middlename { get; set; }
+        public string Position { get; set; }
+        public string Email { get; set; }
 
         public MainWindow()
         {
@@ -47,6 +49,30 @@ namespace AdminSystem
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             
+        }
+
+        private void DataGridAllUsers_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                var items = DataGridAllUsers.CurrentItem as Users;
+
+                if (items == null)
+                {
+                    return;
+                }
+
+                Id = items.Id;
+                Lastname = items.Lastname;
+                Firstname = items.Firstname;
+                Middlename = items.Middlename;
+
+                new FormViewCoursesUser(Id, Lastname, Firstname, Middlename).ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
