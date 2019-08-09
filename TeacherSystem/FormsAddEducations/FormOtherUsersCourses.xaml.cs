1f824da64271a3dad386d0410485732e2fc02cef
MasterFatum <master_fatum@hotmail.com>
2019-08-09 10:56:20 +0300

    AddFormOtherUserCourseFull

--- TeacherSystem/FormsAddEducations/FormOtherUsersCourses.xaml.cs
+++ TeacherSystem/FormsAddEducations/FormOtherUsersCourses.xaml.cs
@@ -3,6 +3,7 @@ using System.Windows;
 using System.Windows.Controls;
 using Bll.Concrete;
 using BLL.Concrete;
+using BLL.Entities;
 
 namespace TeacherSystem.FormsAddEducations
 {
@@ -15,6 +16,13 @@ namespace TeacherSystem.FormsAddEducations
         {
             InitializeComponent();
         }
+
+        public string Lastname { get; set; }
+        public string Category { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Evaluation { get; set; }
+        public string Date { get; set; }
 
         CourseRepository courseRepository = new CourseRepository();
 
@@ -51,6 +59,33 @@ namespace TeacherSystem.FormsAddEducations
                 MessageBox.Show(ex.Message);
             }
             
+        }
+
+        private void DataGridOtherUsersCategory_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            try
+            {
+                var items = DataGridOtherUsersCategory.CurrentItem as Courses;
+
+                if (items == null)
+                {
+                    return;
+                }
+
+                Lastname = CbxOtherUsersCourses.SelectedItem.ToString();
+                Category = items.Category;
+                Title = items.Title;
+                Description = items.Description;
+                Evaluation = items.Evaluation ?? 0;
+                Date = items.Date ?? "Нет даты";
+
+                new FormViewItemsFull(Lastname, Category, Title, Description, Date, Evaluation).ShowDialog();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
