eb39e75971745965006b6eefa144b99b1eea199a
MasterFatum <master_fatum@hotmail.com>
2020-01-22 15:23:00 +0300

    1

--- AdminSystem/Forms/FormSummaryStatement.xaml.cs
+++ AdminSystem/Forms/FormSummaryStatement.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using BLL.Concrete;
 
 namespace AdminSystem.Forms
 {
@@ -24,6 +13,11 @@ namespace AdminSystem.Forms
         private void BtnClose_Click(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            CbxUsersSummaryStatement.ItemsSource = new UserRepository().GetFioUsers();
         }
     }
 }
--- BLL/Abstract/IUserRepository.cs
+++ BLL/Abstract/IUserRepository.cs
@@ -26,8 +26,5 @@ namespace BLL.Abstract
 
         string GetAllUsersCount();
 
-
-
-
     }
 }
